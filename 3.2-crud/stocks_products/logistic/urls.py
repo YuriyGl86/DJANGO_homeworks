b60fad220386d67285eeaa98f2ae84b245c0af18
YuriyGl86 <yuriygl86@gmail.com>
2022-12-06 07:45:42 +0300

    3.2 in progress 2

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from logistic.views import ProductViewSet, StockViewSet
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('stocks', StockViewSet)
+router.register('products', ProductViewSet, basename='product')
+router.register('stocks', StockViewSet, basename='stock')
 
 urlpatterns = router.urls
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -5,9 +6,21 @@ from logistic.serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        search = self.request.GET.get('search')
+        if search:
+            qw_set = Product.objects.filter(Q(title__icontains=search) | Q(description__icontains=search))
+            return qw_set
+
+        if not pk:
+            return Product.objects.all()
+
+        return Product.objects.filter(pk=pk)
 
 
 class StockViewSet(ModelViewSet):
